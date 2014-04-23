098b37abea541b733b3dae95eba64d7ff079e343
deng1919 <deng1919@gmail.com>
2014-04-23 00:10:27 -0700

    Update cachematrix.R
    more clarification

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
 ## The purpose of these functions is to read a matrix and store
-## the matrix and its inverse matrix in the same environment, so
-## that when the same matrix is encountered later, instead of
-## calculating again, the value of its inverse matrix can be obtained
-## from its environment.
+## the matrix and the function for solving its inverse matrix in the 
+## same environment, so that when the same matrix is encountered later, 
+## instead of calculating again, the value of its inverse matrix can be 
+## obtained from the cache environment.
 
-## the makeCacheMatrix is to read in a matrix, and put the matrix
-## in the same environment of the function 'solve' to get its inverse
-## matrix. get is fthe function or store the input matrix, set is the
-## function for updating matrix. getsolve is the function for getting
-## inverse matrix in for the get matrix. setsolve is the function for
-## getting inverse matrix for the updatad matrix stored in set.
+## the makeCacheMatrix is to read in a matrix, and assign several methods (get, 
+## set, getsolve, setsolve) the matrix in the same environment.  The 
+## function get is to get the matrix from cache if it is encountered before 
+## and the getsolve function is to solve the inverse matrix for the matrix in cache.  
+## The function set is to put the matrix into the same environment with its methods.
+## setsolve is the function of solving inverse matrix for the matrix just set 
+## to the environment (ie first encounter).
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -27,12 +28,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## This is the function used to judge if the matrix to be solved
-## has already been there in the cache. If true, it is calling
-## the getsolve function and return the inverse matrix and a message
-## 'getting cached data'. If not, the program is calling get function
-## to read in data and solve the inverse matrix, store that function
-## in cache and return the inverse matrix
+## This is the function used to judge if the matrix has already been there 
+## in the cache. If true, it is calling
+## the getsolve function to solve and return the inverse matrix and a message
+## 'getting cached data'. If not, the program is calling set function
+## to put it in a cache environment and solve the inverse matrix and r
+## eturn the inverse matrix
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
